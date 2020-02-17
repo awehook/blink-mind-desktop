d8c721b88b1c28efd6d1a62411694b473da53c4a
awehook <awehook@163.com>
2020-02-17 17:52:17 +0800

    feat: 优化搜索体验

--- src/renderer/components/toolbar/toolbar-items.tsx
+++ src/renderer/components/toolbar/toolbar-items.tsx
@@ -1,31 +1,33 @@
-import {ToolbarGroupItemUndoRedo, ToolbarItemRedo, ToolbarItemUndo} from "@blink-mind/plugins";
-import {BaseProps} from "@blink-mind/renderer-react";
-import * as React from 'react';
-import {ToolbarItemSearch} from "../../../blink-mind/packages/plugins/src/search/toolbar-item-search";
 import {
+  ToolbarGroupItemUndoRedo,
+  ToolbarItemSearch
+} from '@blink-mind/plugins';
+import {
+  BaseProps,
   ToolbarItemLayout,
   ToolbarItemMore
-} from "../../../blink-mind/packages/renderer-react/src/components/widgets/toolbar";
-import {ToolbarItemViewMode} from "../../plugins/outliner/components/widget";
-import {ToolbarItemTheme} from "../../plugins/toolbar/toolbar-item-theme";
+} from '@blink-mind/renderer-react';
+import * as React from 'react';
+import { ToolbarItemViewMode } from '../../plugins/outliner/components/widget';
+import { ToolbarItemTheme } from '../../plugins/toolbar/toolbar-item-theme';
 import { ToolbarItemGroup } from './toolbar-item-group';
 
-export function ToolbarItems(props:BaseProps) {
+export function ToolbarItems(props: BaseProps) {
   return (
     <>
       <ToolbarItemGroup>
-        <ToolbarItemViewMode {...props}/>
+        <ToolbarItemViewMode {...props} />
       </ToolbarItemGroup>
 
       <ToolbarItemGroup>
-        <ToolbarItemMore {...props}/>
-        <ToolbarItemTheme {...props}/>
-        <ToolbarItemLayout {...props}/>
-        <ToolbarItemSearch {...props}/>
+        <ToolbarItemMore {...props} />
+        <ToolbarItemTheme {...props} />
+        <ToolbarItemLayout {...props} />
+        <ToolbarItemSearch {...props} />
 
-        <ToolbarGroupItemUndoRedo {...props}/>
+        <ToolbarGroupItemUndoRedo {...props} />
       </ToolbarItemGroup>
-      <ToolbarItemGroup/>
+      <ToolbarItemGroup />
     </>
   );
 }
